af588cacb7b10e88ff90c3a7d3c09b2497606d1d
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 15:58:00 +0200

    change username in uppercase in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- Users
 INSERT INTO users (username, password, email)
-VALUES ('mmesander', '$2a$12$RfLgBdcAj/9o/XkMZNm.Zerka9oTm3WRp5nm5rkPg/G5mwVQftbzq', 'mark@test.nl');
+VALUES ('MMESANDER', '$2a$12$RfLgBdcAj/9o/XkMZNm.Zerka9oTm3WRp5nm5rkPg/G5mwVQftbzq', 'mark@test.nl');
 
 -- Authorities
 INSERT INTO authorities (username, authority)
-VALUES ('mmesander', 'ROLE_ADMIN'),
-       ('mmesander', 'ROLE_USER');
+VALUES ('MMESANDER', 'ROLE_ADMIN'),
+       ('MMESANDER', 'ROLE_USER');
