7288635f3cf9d54ffa1665e4cf0d1e0ef145f622
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 15:11:09 +0200

    add security-payload

---
+++ src/main/java/nl/dodo_en_kuik/backend/security/payload/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package nl.dodo_en_kuik.backend.security.payload;
+
+import lombok.Getter;
+
+@Getter
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+}
