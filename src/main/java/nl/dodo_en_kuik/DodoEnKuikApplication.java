5a50210ea4bbb8572d3106b646e36628f5e71f98
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 14:30:25 +0200

    first commit

---
+++ src/main/java/nl/dodo_en_kuik/DodoEnKuikApplication.java
@@ -0,0 +1,13 @@
+package nl.dodo_en_kuik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoEnKuikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoEnKuikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/dodo_en_kuik/DodoEnKuikApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dodo_en_kuik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoEnKuikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
