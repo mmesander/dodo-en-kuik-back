1fd5dfb04c967306ad9e53805d0204d0028698ba
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 14:54:19 +0200

    add exceptions

---
+++ src/main/java/nl/dodo_en_kuik/backend/exceptions/UsernameNotFoundException.java
@@ -0,0 +1,17 @@
+package nl.dodo_en_kuik.backend.exceptions;
+
+// Imports
+import java.io.Serial;
+
+public class UsernameNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public UsernameNotFoundException() {
+        super();
+    }
+
+    public UsernameNotFoundException(String username) {
+        super("Can't find user: " + username);
+    }
+}
