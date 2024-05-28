1fdc6df59d0e6b9d92999454f375edab7eccd2f8
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 15:00:30 +0200

    add input and output dtos

---
+++ src/main/java/nl/dodo_en_kuik/backend/dtos/output/UserDto.java
@@ -0,0 +1,22 @@
+package nl.dodo_en_kuik.backend.dtos.output;
+
+// Imports
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
+import lombok.Setter;
+import nl.dodo_en_kuik.backend.interfaces.IdentifiableUsername;
+import nl.dodo_en_kuik.backend.models.Authority;
+import java.util.Set;
+
+@Getter
+@Setter
+public class UserDto implements IdentifiableUsername {
+    // Variables
+    private String username;
+    private String password;
+    private String email;
+
+    // Relations
+    @JsonSerialize
+    private Set<Authority> authorities;
+}
