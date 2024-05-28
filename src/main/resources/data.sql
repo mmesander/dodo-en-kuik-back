a0352e077d57be6c6073de620f9d6aaf720e9290
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 15:39:17 +0200

    fix typo in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- Users
 INSERT INTO users (username, password, email)
-VALUES ('mmesander', '$2a$12$RfLgBdcAj/9o/XkMZNm.Zerka9oTm3WRp5nm5rkPg/G5mwVQftbzq', 'mark@test.nl'),
+VALUES ('mmesander', '$2a$12$RfLgBdcAj/9o/XkMZNm.Zerka9oTm3WRp5nm5rkPg/G5mwVQftbzq', 'mark@test.nl');
 
 -- Authorities
 INSERT INTO authorities (username, authority)
