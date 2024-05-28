9430a5493ef59d8787ea90c0e56a5466563fad40
Mark Mesander <mark_mesander@hotmail.com>
2024-05-28 14:55:40 +0200

    add specifications

---
+++ src/main/java/nl/dodo_en_kuik/backend/specifications/UserSpecification.java
@@ -0,0 +1,38 @@
+package nl.dodo_en_kuik.backend.specifications;
+
+// Imports
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import nl.dodo_en_kuik.backend.models.User;
+import org.springframework.data.jpa.domain.Specification;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserSpecification implements Specification<User> {
+    private String username;
+    private String email;
+
+    public UserSpecification(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (username != null && !username.isEmpty()) {
+            username = username.toLowerCase();
+            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("username")), "%" + username + "%"));
+        }
+
+        if (email != null && !email.isEmpty()) {
+            email = email.toLowerCase();
+            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), "%" + email + "%"));
+        }
+
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+    }
+}
